a20cf82c8809aa73cbd10d557de879c81d60d454
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-12 17:41:12 +0200

    cleaned and using latest cobalt1.0 stuff

--- cobalt.firebaseAnalytics.js
+++ cobalt.firebaseAnalytics.js
@@ -1,38 +1,27 @@
 (function(cobalt){
     var plugin = {
-        'name': 'firebaseAnalytics',
+        classes: {
+        	ios: 'FirebaseAnalyticsPlugin',
+			android: 'io.kristal.firebaseanalyticscobaltplugin.FirebaseAnalyticsPlugin'
+        }
         init: function (settings) {
             // Create shortcuts
             cobalt.firebaseAnalytics={
                 logEvent: this.logEvent.bind(this)
             };
-
-            if (settings) {
-                this.config(settings);
-            }
         },
-
-        config: function(settings){},
-
         logEvent:function(eventName, eventParams){
 			if (eventName) {
 				var data = {
 					event: eventName,
 					params: eventParams
 				};
-				this.send('logEvent', data);
+				cobalt.plugins.send(this, 'logEvent', data);
 			} else {
 				cobalt.log('no event name for firebaseAnalytics.logEvent')
 			}
 			
         },
-        handleEvent:function(json){
-            cobalt.log(this.name, ' plugin : unknown event received :', json)
-        },
-
-        send:function(action, data, callback){
-            cobalt.send({ type : "plugin", name : this.name, action : action, data : data }, callback);
-        }
     };
     cobalt.plugins.register(plugin);
 })(cobalt || {});
