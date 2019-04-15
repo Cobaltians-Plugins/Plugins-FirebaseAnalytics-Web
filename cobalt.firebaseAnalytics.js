2c1dfcadc5e99e2e214fbb1499565cb7e1c945a1
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:11:23 +0200

    typo

--- cobalt.firebaseAnalytics.js
+++ cobalt.firebaseAnalytics.js
@@ -1,27 +1,26 @@
-(function(cobalt){
-    var plugin = {
-        classes: {
-        	ios: 'FirebaseAnalyticsPlugin',
-			android: 'io.kristal.firebaseanalyticscobaltplugin.FirebaseAnalyticsPlugin'
-        }
-        init: function (settings) {
-            // Create shortcuts
-            cobalt.firebaseAnalytics={
-                logEvent: this.logEvent.bind(this)
-            };
-        },
-        logEvent:function(eventName, eventParams){
-			if (eventName) {
-				var data = {
-					event: eventName,
-					params: eventParams
-				};
-				cobalt.plugins.send(this, 'logEvent', data);
-			} else {
-				cobalt.log('no event name for firebaseAnalytics.logEvent')
-			}
-			
-        },
-    };
-    cobalt.plugins.register(plugin);
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: 'FirebaseAnalyticsPlugin',
+      android: 'io.kristal.firebaseanalyticscobaltplugin.FirebaseAnalyticsPlugin'
+    },
+    init: function(settings) {
+      cobalt.firebaseAnalytics = {
+        logEvent: this.logEvent.bind(this)
+      };
+    },
+    logEvent: function(eventName, eventParams) {
+      if (eventName) {
+        var data = {
+          event: eventName,
+          params: eventParams
+        };
+        cobalt.plugins.send(this, 'logEvent', data);
+      } else {
+        cobalt.log('no event name for firebaseAnalytics.logEvent')
+      }
+
+    },
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
